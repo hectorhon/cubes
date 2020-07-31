615226b08d11550dd4d7a6adb060e7150b824b5d
hectorhon <hon.lianhung@gmail.com>
2020-07-31 17:57:34 +0800

    mocha and should.js

---
+++ server/test/test.js
@@ -0,0 +1,9 @@
+require('should')
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      ([1,2,3].indexOf(4)).should.equal(-1)
+    });
+  });
+});
