05572bb686ace2460fd9e9e731d5f5e5583c6b77
hectorhon <hon.lianhung@gmail.com>
2020-08-02 13:56:59 +0800

    pino

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const Koa = require('koa')
+const pino = require('pino')
 
 const app = new Koa()
+const log = pino()
 
 app.use(ctx => {
+  log.info({ a: 123 }, 'Hello Koa', )
   ctx.body = 'Hello Koa'
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  log.info({ port: 3000 }, 'App started')
+})
